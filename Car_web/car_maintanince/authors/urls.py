54e26dab98529ac6fa27ce2aceb8254941fdec81
sojibhasan5800 <98503808+sojibhasan5800@users.noreply.github.com>
2025-03-01 17:48:29 +0600

    Delete Car_web/car_maintanince directory
    d

--- Car_web/car_maintanince/authors/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import registerpage
-
-urlpatterns = [
-   path('signup/',registerpage,name='signup_page')
-]
--- Car_web/car_maintanince/brand_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BrandAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'brand_app'
