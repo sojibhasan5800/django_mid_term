f6723332629555795421e6a05bc4932d93cc12f7
sojibhasan580 <sojibhasan5800@gmail.com>
2025-05-11 16:31:59 +0600

    m

--- New folder/car_maintance/car_maintance/settings.py
+++ New folder/car_maintance/car_maintance/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'car_maintance.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'car_maintance_db',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'db',   # Docker service name, NOT localhost
+        'PORT': '5432',
     }
 }
 
